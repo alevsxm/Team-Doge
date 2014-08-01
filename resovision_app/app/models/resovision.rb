6df9483e0633d1e99b7bfc483675bbbd97aa36e8
flyptkarsh <ptkarsh@gmail.com>
2014-08-01 13:59:28 -0400

    Revert 021a442..b2a65ee
    This rolls back to commit 021a4426063cb48290de330cc55f561b7f9ac47e.

--- resovision_app/app/models/resovision.rb
+++
@@ -1,6 +0,0 @@
-class Resovision < ActiveRecord::Base
-  belongs_to :user
-  has_many :positions, dependent: :destroy
-  has_many :educations, dependent: :destroy
-  has_one :video, dependent: :destroy 
-end
