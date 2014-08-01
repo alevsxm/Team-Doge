021a4426063cb48290de330cc55f561b7f9ac47e
flyptkarsh <ptkarsh@gmail.com>
2014-08-01 13:59:11 -0400

    Revert 6847880..2ebe08c
    This rolls back to commit 6847880b3cf60b2ab13beb16c2342b56069875c8.

--- resovision_app/app/models/education.rb
+++
@@ -1,4 +0,0 @@
-class Education < ActiveRecord::Base
-  belongs_to :resovision
-  has_one :video  
-end
--- resovision_app/app/models/user.rb
+++
@@ -1,3 +0,0 @@
-class User < ActiveRecord::Base
-  has_one :resovision, dependent: :destroy
-end
