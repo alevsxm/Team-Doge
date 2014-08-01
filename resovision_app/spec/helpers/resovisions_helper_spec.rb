9d9512c258acf8c5ba5ad17e83c3e8d190f05cf3
flyptkarsh <ptkarsh@gmail.com>
2014-08-01 17:12:49 -0400

    users and resources controller created

---
+++ resovision_app/spec/helpers/resovisions_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the ResovisionsHelper. For example:
+#
+# describe ResovisionsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe ResovisionsHelper, :type => :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
