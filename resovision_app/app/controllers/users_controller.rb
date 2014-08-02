80eeaeca1c763e17264265dfc14a88b9c8738794
LizaLemons <liza.h.ramo@gmail.com>
2014-08-02 13:30:19 -0400

    changes to routes and users_controller.rb

--- resovision_app/app/controllers/users_controller.rb
+++ resovision_app/app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authorize, only: [:edit, :update, :destroy]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:linkedin_email, :linkedin_pw)
+  end
+
 end
--- resovision_app/config/routes.rb
+++ resovision_app/config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  #users
-  get 'users/:id' => 'users#show'
+  resources :users, :resovisions, :positions, :educations
 
-  #resources
-  get 'resovisions' => 'resovisions#index'
-  get 'resovisions/:id/edit' => 'resovisions#edit'
-  get 'resovisions/:id' => 'resovisions#show'
+  # Sessions
+  get '/login' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+
+  # #users
+  # get 'users/:id' => 'users#show'
+  #
+  # #resovisions
+  # get 'resovisions' => 'resovisions#index'
+  # get 'resovisions/:id/edit' => 'resovisions#edit'
+  # get 'resovisions/:id' => 'resovisions#show'
 
 end
