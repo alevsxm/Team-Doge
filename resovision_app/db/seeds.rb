b5974b6fb42a52b6467185077dedb19e7a9b3df4
LizaLemons <liza.h.ramo@gmail.com>
2014-08-03 14:38:48 -0400

    filling out first entry in seed file

--- resovision_app/db/seeds.rb
+++ resovision_app/db/seeds.rb
@@ -13,7 +13,7 @@ liza = User.create(
 
 # Resovision:
 liza.resovision.push(
-  Resovision.create(
+  resovision_one = Resovision.create(
   linkedin_id: "b7m0rmBKE7",
   last_name: "Ramo",
   first_name: "Liza",
@@ -25,3 +25,51 @@ liza.resovision.push(
   pic_url: "http://m.c.lnkd.licdn.com/mpr/mprx/0_2bYEOgZpbv-QM7PhC_SJO0w-51vbv2-haiEzO0VisvTBEeL88TmQKxmDwOzZ9I12DkyNAZH527sk",
   user_id: 1
 ))
+
+# Positions:
+resovision_one.positions.push(
+  position_one = Position.create(
+    title: "",
+    summary: "",
+    start_year: "",
+    start_month: "",
+    is_current: "",
+    company_name: "",
+    resovision_id: ""
+  ),
+  position_two = Position.create(
+    title: "",
+    summary: "",
+    start_year: "",
+    start_month: "",
+    is_current: "",
+    company_name: "",
+    resovision_id: ""
+  ),
+  position_three = Position.create(
+    title: "",
+    summary: "",
+    start_year: "",
+    start_month: "",
+    is_current: "",
+    company_name: "",
+    resovision_id: " "
+  )
+)
+
+# Education:
+resovision_one.educations.push(
+  education_one = Education.create(
+    school_name: "",
+    degree: "",
+    start_year: ,
+    end_year: ,
+    resovision_id: 1
+  )
+)
+
+
+
+
+
+###
