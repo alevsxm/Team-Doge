63005abc920fb662541d7d440a8de230cd065b04
flyptkarsh <ptkarsh@gmail.com>
2014-08-04 09:44:21 -0400

    edited resovisions controller

--- resovision_app/app/controllers/resovisions_controller.rb
+++ resovision_app/app/controllers/resovisions_controller.rb
@@ -1,9 +1,21 @@
+require 'pry-rails'
+require 'linkedin'
 class ResovisionsController < ApplicationController
 	#index
 	def index 
 		@resovisions.all 	
 	end 
 	#edit
+	def new 
+		client = LinkedIn::Client.new( '75yxb7noe10oi2', '4soyrz71IfdV9WQZ' )
+		request_token = client.request_token
+		rtoken = request_token.token
+		rsecret = request_token.secret
+		@url =request_token.authorize_url
+		client.authorize_from_access(rtoken, rsecret)
+		 
+	end
+	binding.pry
 	def edit
 		@resovision = Resovision.find('linkedin_id')
 		@educations= Educations.where(id: 'resovision_id')
