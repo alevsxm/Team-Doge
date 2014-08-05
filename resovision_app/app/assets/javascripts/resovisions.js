5fb53ce07145f271457d2724ae2353c8228c2786
flyptkarsh <ptkarsh@gmail.com>
2014-08-05 17:40:49 -0400

    changed the edit.html.erb and partial in resovision

--- resovision_app/app/assets/javascripts/resovisions.js
+++ resovision_app/app/assets/javascripts/resovisions.js
@@ -14,22 +14,20 @@ $(document).ready(function() {
         updateInfo.call(this);
       }
     });
-});
 
-$(function() {
   $('.show-ziglet').on('click', function(){
     var player = $(this).parent().find('.player');
     console.log(player);
 
       player.css('visibility', 'visible');
-  })
+  });
   $('.record-ziglet').on('click', function(){
     var recorder = $(this).parent().find('.recorder');
     console.log(recorder);
 
       recorder.css('visibility', 'visible');
-  })
-
+  }); 
+    
 });
 
 function editInfo() {
