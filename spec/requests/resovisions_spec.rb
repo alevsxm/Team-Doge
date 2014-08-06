ef4ff02f23ad3df79abd22e2f68d98bb23dfeb35
flyptkarsh <ptkarsh@gmail.com>
2014-08-06 16:27:35 -0400

    Working on Capybara and rspec

--- spec/requests/resovisions_spec.rb
+++ spec/requests/resovisions_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "Tasks", :type => :request do
-  describe "GET /tasks" do
+RSpec.describe "Resovisions", :type => :request do
+  describe "GET /resovisions" do
     it "works! (now write some real specs)" do
-      get tasks_path
+      get resovisions_path
       expect(response.status).to be(200)
     end
   end
